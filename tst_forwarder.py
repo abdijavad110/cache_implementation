c0859b6bd1bab72c098b00cdf791da6fcebc58f9
javad abdi <abdijavad110@gmail.com>
2019-12-27 18:11:36 +0330

    create forwarder test module

---
+++ tst_forwarder.py
@@ -0,0 +1,29 @@
+import unittest
+from time import sleep
+from random import randint
+
+from forwarder import Forwarder, Request
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        try:
+            ff = Forwarder()
+
+            for _ in range(2000):
+                ff.issue(
+                    Request('/dev/ram0', randint(0, 1), randint(0, 1024 * 1024 * 500), length=512, data=b'\xcd' * 512))
+            sleep(0.5)
+            lat = 0
+            for i in range(2000):
+                res = ff.res_Q.pop()
+                lat = (lat * i + res.latency) / (i + 1)
+                # print(res.addr, res.type, res.latency * 1000)
+            print("\nmean latency = %f" % (lat * 1000))
+            self.assertTrue('mean latency = %f' % (lat * 1000))
+        except Exception as e:
+            self.assertFalse(e.__traceback__)
+
+
+if __name__ == '__main__':
+    unittest.main()
