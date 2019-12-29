3f1d3f515144ffac1164bd338ce9a1680a6230f4
javad abdi <abdijavad110@gmail.com>
2019-12-29 21:21:36 +0330

    new: dev: add trace status

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import threading
+from tqdm import tqdm
 
 from parser import Parser
 from forwarder import Forwarder
 
 
 if __name__ == '__main__':
-    parser1 = Parser('trace/NEW_2016031907-LUN1.csv')
-    parser2 = Parser('trace/NEW_2016031907-LUN4.csv')
+    parsers = [
+        Parser('trace/NEW_2016031907-LUN1.csv'),
+        Parser('trace/NEW_2016031907-LUN4.csv')
+    ]
     forwarder = Forwarder(concurrency=10)
 
-    for parser in [parser1, parser2]:
+    llt = 0
+    for parser in parsers:
+        llt += parser.cnt
         threading.Thread(
             target=parser.start_sending_requests,
             args=(forwarder.issue,),
@@ -17,12 +22,19 @@ if __name__ == '__main__':
         ).start()
 
     lats = []
-    try:
-        while True:
-            res = forwarder.res_Q.pop()
-            lats.append(res.latency*1000)
-            # print("%s in %d ==> %fms" % (res.type, res.addr, res.latency*1000))
-    except KeyboardInterrupt:
-        print("\n\naverage latency= %.3fms" % (sum(lats)/len(lats)))
-        print("maximum latency= %.3fms" % max(lats))
-        print("minimum latency= %.3fms\n" % min(lats))
+    cnt = 0
+    with tqdm(total=llt) as pbar:
+        try:
+            while True:
+                cnt += 1
+                if cnt == 500:
+                    pbar.update(cnt)
+                    cnt = 0
+                res = forwarder.res_Q.pop()
+                lats.append(res.latency*1000)
+
+        except KeyboardInterrupt:
+            pbar.update(cnt)
+            print("\n\naverage latency= %.3fms" % (sum(lats)/len(lats)))
+            print("maximum latency= %.3fms" % max(lats))
+            print("minimum latency= %.3fms\n" % min(lats))
