72377ef7be604e59b5ee1367e3cc8f9d65b98ad2
javad abdi <abdijavad110@gmail.com>
2019-12-29 17:57:32 +0330

    new: dev: implement proxy class (part 1)

--- conf.py
+++ conf.py
@@ -16,4 +16,23 @@ class CacheConf:
     SSDSize = 1024*1024*1024*4
     RAMSize = 1024*1024*1024*1
 
+    HDD = '/dev/sda99'
+    SSD = '/dev/sdb99'
+    RAM = '/dev/ram0'
+
     BLK_SIZE = 4096
+
+    ram_blk = 0
+    ssd_blk = 1
+    hdd_blk = 2
+
+    @staticmethod
+    def get_dev(typ):
+        if typ == CacheConf.ssd_blk:
+            return CacheConf.SSD
+        elif typ == CacheConf.ram_blk:
+            return CacheConf.RAM
+        elif typ == CacheConf.hdd_blk:
+            return CacheConf.HDD
+        else:
+            raise Exception("Unknown device type!")
--- proxy.py
+++ proxy.py
@@ -1,19 +1,29 @@
 from conf import CacheConf
 from mapping import Tables
+from forwarder import Forwarder, Request
 
 
 class Proxy:
+    forwarder = Forwarder()
+
     def __init__(self):
+        raise Exception('Proxy is a static class, so can not be instantiated.')
+
+    @staticmethod
+    def invalidate(blk):
+        if blk.typ == CacheConf.ram_blk:
+            Tables.RAM.invalidate(blk.addr)
+        elif blk.typ == CacheConf.ssd_blk:
+            Tables.SSD.invalidate(blk.addr)
+
+    @staticmethod
+    def promote(blk, dst):
         pass
 
-    def invalidate(self, blk):
-        pass
+    @staticmethod
+    def write(blk):
+        Proxy.forwarder.issue(Request(CacheConf.get_dev(blk.typ), blk.typ, blk.addr, data=b'\x00' * blk.len))
 
-    def promote(self, blk, dst):
-        pass
-
-    def write(self, blk):
-        pass
-
-    def read(self, blk):
-        pass
+    @staticmethod
+    def read(blk):
+        Proxy.forwarder.issue(Request(CacheConf.get_dev(blk.typ), blk.typ, blk.addr, length=blk.len))
